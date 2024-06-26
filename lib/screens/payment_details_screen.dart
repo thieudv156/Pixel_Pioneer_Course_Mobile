bd13fc7961fefd932371b6af24dee99a4a5f7e3f
Van Thieu <thieudvts2208034@fpt.edu.vn>
2024-06-26 18:22:47 +0700

    Update view enrollment detail by user

--- lib/screens/payment_details_screen.dart
+++ lib/screens/payment_details_screen.dart
@@ -1,19 +1,22 @@
-import 'package:course_template/utils/PublicBaseURL.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../utils/PublicBaseURL.dart';
 import '../utils/date_utils.dart';
 
 class PaymentDetailScreen extends StatefulWidget {
+  const PaymentDetailScreen({super.key});
+
   @override
   _PaymentDetailScreenState createState() => _PaymentDetailScreenState();
 }
 
 class _PaymentDetailScreenState extends State<PaymentDetailScreen> {
   List<dynamic> _enrollments = [];
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -22,17 +25,26 @@ class _PaymentDetailScreenState extends State<PaymentDetailScreen> {
   }
 
   Future<void> _fetchEnrollments() async {
+    setState(() {
+      _isLoading = true;
+    });
     SharedPreferences prefs = await SharedPreferences.getInstance();
     int? userId = prefs.getInt('userId');
     if (userId != null) {
-      final response =
-          await http.get(Uri.parse('$baseUrl/api/enrollments/user/$userId'));
+      final response = await http.get(Uri.parse(
+          '$baseUrl/api/enrollments/get-user-enrollments?userId=$userId'));
       if (response.statusCode == 200) {
         setState(() {
           _enrollments = jsonDecode(response.body);
+          _isLoading = false;
         });
       } else {
-        // Handle error
+        setState(() {
+          _isLoading = false;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to fetch enrollments')),
+        );
       }
     }
   }
@@ -40,37 +52,44 @@ class _PaymentDetailScreenState extends State<PaymentDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple[100],
-      // Set background color to light purple
+      backgroundColor: Colors.purple[300],
       appBar: AppBar(
-        title: Text('Payment Detail'),
+        title: const Text('Enrollment Detail'),
       ),
-      body: ListView.builder(
-        itemCount: _enrollments.length,
-        itemBuilder: (context, index) {
-          String? paymentDate = formatDate(_enrollments[index]['paymentDate']);
-          String? paymentMethod = _enrollments[index]['paymentMethod'];
-          String? subscriptionType = _enrollments[index]['subscriptionType'];
-          String? subscriptionEndDate =
-              formatDate(_enrollments[index]['subscriptionEndDate']);
-          return Card(
-            margin: EdgeInsets.all(8.0),
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Payment Date: ${paymentDate ?? 'Unknown'}'),
-                  Text('Payment Method: ${paymentMethod ?? 'Unknown'}'),
-                  Text('Subscription Type: ${subscriptionType ?? 'Unknown'}'),
-                  Text(
-                      'Subscription End Date: ${subscriptionEndDate ?? 'Unknown'}'),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : _enrollments.isEmpty
+              ? const Center(child: Text('No data'))
+              : ListView.builder(
+                  itemCount: _enrollments.length,
+                  itemBuilder: (context, index) {
+                    Map<String, dynamic> enrollment = _enrollments[index];
+                    String? paymentDate = formatDate(enrollment['paymentDate']);
+                    String? paymentMethod = enrollment['paymentMethod'];
+                    String? subscriptionType = enrollment['subscriptionType'];
+                    String? subscriptionEndDate =
+                        formatDate(enrollment['subscriptionEndDate']);
+                    return Card(
+                      margin: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        leading: const Icon(Icons.payment),
+                        title:
+                            Text('Payment Date: ${paymentDate ?? 'Unknown'}'),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                'Payment Method: ${paymentMethod ?? 'Unknown'}'),
+                            Text(
+                                'Subscription Type: ${subscriptionType ?? 'Unknown'}'),
+                            Text(
+                                'Subscription End Date: ${subscriptionEndDate ?? 'Unknown'}'),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
     );
   }
 }
